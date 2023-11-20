309d2404e0985190acb4c48b671a9693b29b7710
jacob-turner2 <jacob.turner2@westpoint.edu>
2023-11-20 11:47:43 -0500

    Php should link

--- index.php
+++ index.php
@@ -2,6 +2,22 @@
 // Set the content type to HTML
 header("Content-Type: text/html");
 
-// Include the HTML content
-include("homepage.html");
+// Get the page parameter from the URL
+$page = isset($_GET['page']) ? $_GET['page'] : '';
+
+// Include the appropriate HTML file based on the page parameter
+switch ($page) {
+    case 'faq':
+        include("faq.html");
+        break;
+    case 'gallery':
+        include("gallery.html");
+        break;
+    case 'request':
+        include("request.html");
+        break;
+    default:
+        include("homepage.html");
+}
+
 ?>
